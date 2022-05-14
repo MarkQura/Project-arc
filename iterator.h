f1dca43ee2a3198d701f75b0424dcaed783aa011
MarkQura <Prision09@gmail.com>
2022-05-14 20:26:53 +0100

    Iterator Update

---
+++ iterator.h
@@ -0,0 +1,44 @@
+#ifndef ITERATOR_H
+#define ITERATOR_H
+
+typedef struct _iterator * iterator; 
+
+/***********************************************
+criaIterador - Criacao da instancia da estrutura associada a um iterador para uma lista ligada com n elementos.
+Parametros:
+	cabeça - endereco da copia da cabeça lista ligada a iterar
+    cauda - endereco da copia da cauda lista ligada a iterar
+	n - numero de elementos no vector
+Retorno: apontador para a  instancia criada
+Pre-condicoes:  vector != NULL && n > 0
+***********************************************/
+iterator new_iterator(node head, node tail, int n, int mode);
+
+/***********************************************
+destroiIterador - Liberta a memoria ocupada pela instancia da estrutura associada ao iterador.
+Parametros:
+	it - iterador a destruir
+Retorno:
+Pre-condicoes: it != NULL
+***********************************************/
+void destroi_iterator(iterator it);
+
+/***********************************************
+temSeguinteIterador - Indica se existe mais elementos para iterar no iterador.
+Parametros:
+	it - iterador
+Retorno: 1 - caso exista mais elementos; 0 - caso contrario
+Pre-condicoes: it != NULL
+***********************************************/
+int has_next_item(iterator it);
+
+/***********************************************
+seguinteIterador - Consulta o seguinte elemento no iterador.
+Parametros:
+	it - iterador
+Retorno: enderco do elemento
+Pre-condicoes: it != NULL && temSeguinteIterador(it) == 1
+***********************************************/
+void* next_item(iterator it);
+
+#endif // ITERATOR_H
---
+++ linkedIterator.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "iterator.h"
+#include "Node.h"
+
+struct _iterador {
+    node head, tail, item;
+    int size, reverse, current;
+};
+
+iterator new_iterador(node head, node tail, int size, int reverse) {
+    iterator it = (iterator) malloc(sizeof(struct _iterador));
+    if (it == NULL) return NULL;
+    it->head = head;
+    it->tail = tail;
+    it->size = size;
+    it->reverse = reverse;
+    it->current = 0;
+    it->item = reverse ? tail : head;
+    return it;
+}
+
+void destroi_iterator(iterator it) {
+    destroiNode(it->head);
+    destroiNode(it->tail);
+    free(it);
+}
+
+int has_next_item(iterator it) {
+    if (it->current == it->size) return 0;
+    return 1;
+}
+
+void* next_item(iterator it) {
+    it->item = (it->reverse * previousNode(it->item)) + (!(it->reverse) * nextNode(it->item));
+    it->current++;
+    return getData(it->item);
+}
