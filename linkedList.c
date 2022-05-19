757aa9f3c281271480a34e9985f758898b8f0b28
MarkQura <69739495+MarkQura@users.noreply.github.com>
2022-05-19 15:30:42 +0100

    linked update

--- linkedList.c
+++ linkedList.c
@@ -5,12 +5,14 @@
 #include "iterator.h"
 #include "linkedList.h"
 
-struct _linkedList{
+struct _linkedList
+{
     node head, tail;
     int nElemsTotal, nCertifiedElems;
 };
 
-linkedList newList() {
+linkedList newList()
+{
     linkedList list = (linkedList)malloc(sizeof(struct _linkedList));
     list->head = NULL;
     list->tail = NULL;
@@ -19,38 +21,45 @@ linkedList newList() {
     return list;
 }
 
-void destroyList(linkedList list){
+void destroyList(linkedList list)
+{
     node aux = list->head;
-	while (aux != NULL){
-		list->head = nextNode(aux);
-		destroyNode(aux);
-		aux = list->head;
-	}
-	free(list);
+    while (aux != NULL)
+    {
+        list->head = nextNode(aux);
+        destroyNode(aux);
+        aux = list->head;
+    }
+    free(list);
 }
 
-void destroyListAndElems(linkedList list, void (*destroy)(void *)){
-	node aux = list->head;
-	while (aux != NULL){
-		list->head = nextNode(aux);
-		destroyNodeAndElems(aux, destroy);
-		aux = list->head;
-	}
-	free(list);
+void destroyListAndElems(linkedList list, void (*destroy)(void *))
+{
+    node aux = list->head;
+    while (aux != NULL)
+    {
+        list->head = nextNode(aux);
+        destroyNodeAndElems(aux, destroy);
+        aux = list->head;
+    }
+    free(list);
 }
 
-node locateIndex(linkedList list, int index){
+node locateIndex(linkedList list, int index)
+{
     node aux = aux->head;
-    for(int i = 0; i < index; ++i)
+    for (int i = 0; i < index; ++i)
         aux = nextNode(aux);
     return aux;
 }
 
-void addHead(linkedList list, void* elem){
+void addHead(linkedList list, void *elem)
+{
     node head = newNode(elem);
     list->nElemsTotal++;
     setPrevNode(head, NULL);
-    if(!list->nElemsTotal){
+    if (!list->nElemsTotal)
+    {
         list->head = head;
         list->tail = head;
         setNextNode(head, NULL);
@@ -61,29 +70,34 @@ void addHead(linkedList list, void* elem){
     list->head = head;
 }
 
-void insert(linkedList list, void* elem, int index){
-    if(!index){
+void insert(linkedList list, void *elem, int index)
+{
+    if (!index)
+    {
         addHead(list, elem);
         return;
     }
-    if(index == list->nCertifiedElems){
+    if (index == list->nCertifiedElems)
+    {
         append(list, elem);
         return;
     }
-    node newNode = newNode(elem);
+    node nNode = newNode(elem);
     list->nElemsTotal++;
     node aux = locateIndex(list, index);
-    setNextNode(newNode, aux);
-    setNextNode(prevNode(aux), newNode);
-    setPrevNode(newNode, prevNode(aux));
-    setPrevNode(aux, newNode);
+    setNextNode(nNode, aux);
+    setNextNode(prevNode(aux), nNode);
+    setPrevNode(nNode, prevNode(aux));
+    setPrevNode(aux, nNode);
 }
 
-void append(linkedList list, void* elem){
+void append(linkedList list, void *elem)
+{
     node tail = newNode(elem);
     list->nElemsTotal++;
     setNextNode(tail, NULL);
-    if(!list->nElemsTotal){
+    if (!list->nElemsTotal)
+    {
         list->head = tail;
         list->tail = tail;
         setPrevNode(tail, NULL);
@@ -94,11 +108,13 @@ void append(linkedList list, void* elem){
     list->tail = tail;
 }
 
-void moveToHead(linkedList list, node head){
+void moveToHead(linkedList list, node head)
+{
     setNextNode(prevNode(head), nextNode(head));
     setPrevNode(nextNode(head), prevNode(head));
     setPrevNode(head, NULL);
-    if(!list->nElemsTotal){
+    if (!list->nElemsTotal)
+    {
         list->head = head;
         list->tail = head;
         setNextNode(head, NULL);
@@ -109,8 +125,10 @@ void moveToHead(linkedList list, node head){
     list->head = head;
 }
 
-void moveToIndex(linkedList list, node moveNode, int index){
-    if(!index){
+void moveToIndex(linkedList list, node moveNode, int index)
+{
+    if (!index)
+    {
         moveHead(list, moveNode);
         return;
     }
@@ -123,11 +141,13 @@ void moveToIndex(linkedList list, node moveNode, int index){
     setNextNode(moveNode, aux);
 }
 
-void moveToTail(linkedList list, node tail){
+void moveToTail(linkedList list, node tail)
+{
     setNextNode(prevNode(tail), nextNode(tail));
     setPrevNode(nextNode(tail), prevNode(tail));
     setNextNode(tail, NULL);
-    if(!list->nElemsTotal){
+    if (!list->nElemsTotal)
+    {
         list->head = tail;
         list->tail = tail;
         setPrevNode(tail, NULL);
@@ -138,50 +158,59 @@ void moveToTail(linkedList list, node tail){
     list->tail = tail;
 }
 
-int sizeList(linkedList list){
+int sizeList(linkedList list)
+{
     return list->nElemsTotal;
 }
 
-int sizeCertified(linkedList list){
+int sizeCertified(linkedList list)
+{
     return list->nCertifiedElems;
 }
 
-int sizeBanned(linkedList list){
+int sizeBanned(linkedList list)
+{
     return (list->nElemsTotal - list->nCertifiedElems);
 }
 
-node getHead(linkedList list){
+node getHead(linkedList list)
+{
     return (list->head);
 }
 
-node getTail(linkedList list){
+node getTail(linkedList list)
+{
     return (list->tail);
 }
 
-iterator certifiedIterator(linkedList list){
+iterator certifiedIterator(linkedList list)
+{
     node auxHead = newNode(getElem(list->head));
     setNextNode(auxHead, nextNode(list->head));
     node auxTail = newNode(getElem(list->tail));
     setPrevNode(auxHead, prevNode(list->tail));
     iterator it = new_iterator(auxHead, auxTail, list->nCertifiedElems, 0);
-	return it;
+    return it;
 }
 
-iterator listIterator(linkedList list){
+iterator listIterator(linkedList list)
+{
     node auxHead = newNode(getElem(list->head));
     setNextNode(auxHead, nextNode(list->head));
     node auxTail = newNode(getElem(list->tail));
     setPrevNode(auxHead, prevNode(list->tail));
     iterator it = new_iterator(auxHead, auxTail, list->nElemsTotal, 0);
-	return it;
+    return it;
 }
 
-//split the linked list in the middle
-node partition(node head) {
+// split the linked list in the middle
+node partition(node head)
+{
     node fast, slow;
     fast = slow = head;
 
-    while(nextNode(fast) != NULL && nextNode(nextNode(fast)) != NULL) {
+    while (nextNode(fast) != NULL && nextNode(nextNode(fast)) != NULL)
+    {
         fast = nextNode(nextNode(fast));
         slow = nextNode(slow);
     }
@@ -191,56 +220,67 @@ node partition(node head) {
     return fast;
 }
 
-//junta as partições da lista ligada por ordem
-node merge(node first, node second, int (*getScore) (void* elem)) {
-    if (first == NULL) return second;
-    if (second == NULL) return first;
+// junta as partições da lista ligada por ordem
+node merge(node first, node second, int (*getScore)(void *elem))
+{
+    if (first == NULL)
+        return second;
+    if (second == NULL)
+        return first;
 
-    if (getScore(getElem(first)) < getScore(getElem(second))) { 
-        setNextNode(first, merge(nextNode(first), second));
+    if (getScore(getElem(first)) < getScore(getElem(second)))
+    {
+        setNextNode(first, merge(nextNode(first), second, getScore));
         setPrevNode(nextNode(first), first);
         setPrevNode(first, NULL);
         return first;
-    } else {
-        setNextNode(second, merge(first, nextNode(second)));
+    }
+    else
+    {
+        setNextNode(second, merge(first, nextNode(second), getScore));
         setPrevNode(nextNode(second), second);
         setPrevNode(second, NULL);
         return second;
     }
 }
 
-node mergeSort(node head, int (*getScore) (void* elem)) {
-	if (head == NULL || nextNode(head) == NULL)
+node mergeSort(node head, int (*getScore)(void *elem))
+{
+    if (head == NULL || nextNode(head) == NULL)
         return head;
-    
+
     node second = partition(head);
 
     head = mergeSort(head, getScore);
     second = mergeSort(second, getScore);
 
     return merge(head, second, getScore);
-
 }
 
-void destroyBanned(linkedList list, void (*destroy) (void* elem)) {
+void destroyBanned(linkedList list, void (*destroy)(void *elem))
+{
     int banned = list->nElemsTotal - list->nCertifiedElems;
     node aux = list->tail;
-    for (int i = 0; i < banned; ++i) {
-		list->tail = prevNode(aux);
-		destroyElemAndNode(aux, destroy);
-		aux = list->head;
-	}
+    for (int i = 0; i < banned; ++i)
+    {
+        list->tail = prevNode(aux);
+        destroyNodeAndElems(aux, destroy);
+        aux = list->head;
+    }
 }
 
-void sortList(linkedList list, int (*getScore) (void* elem), void (*destroy) (void* elem)) {
+void sortList(linkedList list, int (*getScore)(void *elem), void (*destroy)(void *elem))
+{
     destroyBanned(list, destroy);
     list->head = mergeSort(list->head, getScore);
 }
 
-node existElem(linkedList list, char* name, char* (*getName) (void*)) {
+node existElem(linkedList list, char *name, char *(*getName)(void *))
+{
     node aux = list->head;
-    while(aux != NULL){
-        if(!strcmp(name, getName(getElem(aux))))
+    while (aux != NULL)
+    {
+        if (!strcmp(name, getName(getElem(aux))))
             return aux;
         aux = nextNode(aux);
     }
