f688080435bd9507d5c38f00b1a589126934625a
MarkQura <69739495+MarkQura@users.noreply.github.com>
2022-05-19 13:55:11 +0100

    bug fixes

--- archaeologist.c
+++ archaeologist.c
@@ -1,64 +1,78 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "archaeologist.h"
 
-struct _arc{
-    name[40];
+struct _arc
+{
+    char name[40];
     int score, certeficate, penalty;
     int pos[2];
 };
 
-arc newArc(){
-    arc aux = (arc*)malloc(sizeof(struct _arc));
+arc newArc()
+{
+    arc aux = (arc)malloc(sizeof(struct _arc));
     if (aux == NULL)
         return NULL;
     aux->score = 0;
     aux->certeficate = 1;
     aux->penalty = 0;
-    aux->pos = {-1,-1};
+    aux->pos[0] = -1;
+    aux->pos[1] = -1;
     return aux;
 }
 
-void destroyAcr(arc aux){
+void destroyAcr(arc aux)
+{
     free(aux);
 }
 
-char* getName(arc aux){
+char *getName(arc aux)
+{
     return aux->name;
 }
 
-int getCertificate(arc aux){
+int getCertificate(arc aux)
+{
     return aux->certeficate;
 }
 
-int getScore(arc aux){
+int getScore(arc aux)
+{
     return aux->score;
 }
 
-int getPenalty(arc aux){
+int getPenalty(arc aux)
+{
     return aux->penalty;
 }
 
-int* getPos(arc aux){
+int *getPos(arc aux)
+{
     return aux->pos;
 }
 
-char* getNameGen(void* aux){
-    return (arc)aux->name;
+char *getNameGen(void *aux)
+{
+    return getName((arc)aux);
 }
 
-void getNewPos(arc aux, int c, int l){
+void getNewPos(arc aux, int c, int l)
+{
     aux->pos[0] += c;
     aux->pos[1] += l;
 }
 
-void addPenalty(arc aux, int penalty){
+void addPenalty(arc aux, int penalty)
+{
     aux->penalty += penalty;
 }
 
-void addScore(arc aux, int score){
+void addScore(arc aux, int score)
+{
     aux->score += score;
 }
 
-void desqualify(arc aux){
+void desqualify(arc aux)
+{
     aux->certeficate = 0;
 }
--- team.c
+++ team.c
@@ -1,30 +1,42 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "team.h"
 #include "linkedList.h"
 #include "node.h"
 #include "archaeologist.h"
 #include "iterator.h"
+#include "team.h"
 
-struct _team {
-    char* name;
+struct _team
+{
+    char *name;
     int score, isBanned;
     node current;
     arc star;
     linkedList archaeologists;
 };
 
-team new_team(char* name) {
+team new_team(char *name)
+{
     team t = malloc(sizeof(struct team));
-    if (t == NULL) return NULL;
+    if (t == NULL)
+        return NULL;
 
-    t->name = (char*) malloc(sizeof(char)*strlen(name));
-    if (t->name == NULL) { free(t); return NULL; }
+    t->name = (char *)malloc(sizeof(char) * strlen(name));
+    if (t->name == NULL)
+    {
+        free(t);
+        return NULL;
+    }
     strncpy(t->name, name, 40);
 
     t->archaeologists = newList(0);
-    if (t->archaeologists == NULL) { free(t->name); free(t); return NULL; }
+    if (t->archaeologists == NULL)
+    {
+        free(t->name);
+        free(t);
+        return NULL;
+    }
 
     t->isBanned = 0;
     t->score = 0;
@@ -34,74 +46,91 @@ team new_team(char* name) {
     return t;
 }
 
-void destroy_team(team t) {
+void destroy_team(team t)
+{
     free(t->name);
-    if (t->star != NULL) destroyArc(t->star);
-    if (t->current != NULL) destroyNode(t->current);
+    if (t->star != NULL)
+        destroyArc(t->star);
+    if (t->current != NULL)
+        destroyNode(t->current);
     destroyList(t->archaeologists);
     free(t);
 }
 
-void destroy_team_and_elems(team t) {
+void destroy_team_and_elems(team t)
+{
     free(t->name);
-    if (t->star != NULL) destroyArc(t->star);
-    if (t->current != NULL) destroyNode(t->current);
+    if (t->star != NULL)
+        destroyArc(t->star);
+    if (t->current != NULL)
+        destroyNode(t->current);
     destroyListAndElems(t->archaeologists, destroyArcGen);
     free(t);
 }
 
-void destroy_team_gen(void* t) {
-    destroy_team((team) t);
+void destroy_team_gen(void *t)
+{
+    destroy_team((team)t);
 }
 
-void destroy_team_and_elems_gen(void* t) {
-    destroy_team_and_elems((team) t);
+void destroy_team_and_elems_gen(void *t)
+{
+    destroy_team_and_elems((team)t);
 }
 
-void add_arc(team t, char* arcName) {
+void add_arc(team t, char *arcName)
+{
     arc a = newArchaeologist(arcName);
     insert(t->archaeologists, a, sizeCertified(t->archaeologists));
 }
 
-char* team_name(team t)
+char *team_name(team t)
 {
     return t->name;
 }
 
-char* team_name_gen(void* t) {
-    return team_name((team) t);
+char *team_name_gen(void *t)
+{
+    return team_name((team)t);
 }
 
-int get_is_banned(team t) {
+int get_is_banned(team t)
+{
     return t->isBanned;
 }
 
-int get_is_banned_gen(void* t) {
-    return get_is_banned((team) t);
+int get_is_banned_gen(void *t)
+{
+    return get_is_banned((team)t);
 }
 
-arc get_star(team t) {
+arc get_star(team t)
+{
     return t->star;
 }
 
-arc get_act(team t) {
-    return (arc) getElem(t->current);
+arc get_act(team t)
+{
+    return (arc)getElem(t->current);
 }
 
-void update_star(team t) {
-    t->star = (arc) getElem(t->current);
+void update_star(team t)
+{
+    t->star = (arc)getElem(t->current);
 }
 
 void next_archaeologist(team t, int pointsMade)
 {
-    if (pointsMade < 0) {
+    if (pointsMade < 0)
+    {
         addPenalty(t->current);
     }
 
     addScore(t->current, pointsMade);
     t->score += pointsMade;
 
-    if (!getCertificate((arc) getElem(nextNode(t->current)))) {
+    if (!getCertificate((arc)getElem(nextNode(t->current))))
+    {
         t->current = getHead(t->archaeologists);
         return;
     }
@@ -109,32 +138,37 @@ void next_archaeologist(team t, int pointsMade)
     t->current = nextNode(t->current);
 }
 
-void ban_elem(team t) {
+void ban_elem(team t)
+{
     node n = nextNode(t->current);
 
     if (n == NULL)
         n = getHead(t->archaeologists);
-    else if (!getCertificate((arc) getElem(n)))
+    else if (!getCertificate((arc)getElem(n)))
         n = getHead(t->archaeologists);
 
     t->score -= getScore(t->current);
-    disqualify((arc) getElem(t->current));
-        
+    disqualify((arc)getElem(t->current));
+
     moveToTail(t->current);
     t->current = n;
 
-    if (sizeCertified(t->archaeologists) == 0) {
+    if (sizeCertified(t->archaeologists) == 0)
+    {
         t->isBanned = 1;
         return;
     }
 
-    if (!strcmp(arcName((arc) getElem(n), arcName(t->star)))) {
+    if (!strcmp(arcName((arc)getElem(n), arcName(t->star))))
+    {
         iterator it = listIterator(t->archaeologists);
-        if (it == NULL && !has_next_item(it)) return;
+        if (it == NULL && !has_next_item(it))
+            return;
         arc a;
-        t->sar = next_item(it);
+        t->star = next_item(it);
 
-        while(has_next_item(it)) {
+        while (has_next_item(it))
+        {
             a = next_item(it);
             if (getScore(t->star) < getScore(a))
                 t->star = a;
@@ -142,24 +176,28 @@ void ban_elem(team t) {
     }
 }
 
-arc exist_arc(team t, char* name) {
-    arc a = (arc) getElem(existElem(t->archaeologists, name, getNameGen));
+arc exist_arc(team t, char *name)
+{
+    arc a = (arc)getElem(existElem(t->archaeologists, name, getNameGen));
     return a;
 }
 
-void set_ban_team(team t) {
+void set_ban_team(team t)
+{
     t->isBanned = 1;
 }
 
-int total_number(team t) {
+int total_number(team t)
+{
     return sizeList(t->archaeologists);
 }
 
-int arc_number(team t) {
+int arc_number(team t)
+{
     return sizeCertified(t->archaeologists);
 }
 
-iterator team_iterator(team t) {
+iterator team_iterator(team t)
+{
     return listIterator(t->archaeologists);
 }
-
